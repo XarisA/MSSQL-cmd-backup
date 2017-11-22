c59228a3402d8161a884996f58c173e72f5b97ea
Xaris <kayaku@protonmail.com>
2017-11-22 14:49:45 +0200

    Add query to test before sqlcmd

---
+++ backup database query.sql
@@ -0,0 +1,12 @@
+/*
+Query to back up databases.
+Used it for test before applying it to sqlcmd
+You don't need extra permissions in C:\Program Files\Microsoft SQL Server
+*/
+BACKUP DATABASE DBNAME1 TO DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.APHRODITE\MSSQL\Backup\DBNAME1.BAK'
+WITH NOFORMAT, INIT, NAME = N'DBNAME1 Back Up' , SKIP, NOREWIND, NOUNLOAD, STATS = 10
+
+BACKUP DATABASE DBNAME2 TO DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL10_50.APHRODITE\MSSQL\Backup\DBNAME2.BAK'
+WITH NOFORMAT, INIT, NAME = N'DBNAME2 BackUp' , SKIP, NOREWIND, NOUNLOAD, STATS = 10
+
+GO
